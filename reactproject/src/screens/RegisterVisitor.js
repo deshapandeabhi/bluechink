af7389ef17ea5b5a3ae0bb5bad5d2119ff5acc18
abhicrezam <abhishek@crezam.com>
2025-01-04 13:16:46 +0530

    Learning Integrations 3

--- reactproject/src/screens/RegisterVisitor.js
+++ reactproject/src/screens/RegisterVisitor.js
@@ -12,7 +12,7 @@ const RegistrationOfVisitor = () => {
   const [company, setCompany] = useState('');
   const [department, setDepartment] = useState('');
   const [address, setAddress] = useState('');
-  console.log('Checking conflicts 1');
+  console.log('Checking conflicts 3');
   console.log('Checking conflicts 2');
   const handleHomeClick = () => {
     // Handle the navigation to "1(1)" here
